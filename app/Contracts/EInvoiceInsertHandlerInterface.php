448d030994bfffbb16cb07bf4b760c50b218fdac
leejungyan08 <jungyan.lee@polyware.com.my>
2025-07-03 14:44:26 +0800

    fix error

--- app/Contracts/EInvoiceInsertHandlerInterface.php
+++ app/Contracts/EInvoiceInsertHandlerInterface.php
@@ -10,11 +10,9 @@ interface EInvoiceInsertHandlerInterface
 
     public function updateToInProgress(): void;
 
-<<<<<<< HEAD
-    public function delete(?string $remark, ?int $staff_id, int $delete_user_id, bool $update_approve_information, bool $from_einvoice): void;
-=======
+
     public function delete(?string $remark, ?int $staff_id, int $delete_user_id, bool $update_approve_information, bool $from_einvoice = false): void;
->>>>>>> 31ee59d4799acbe5da1baa47096b34a754c70b29
+
 
 }
 
