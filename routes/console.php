9fd5fd6e9ce9cc5631d7db5013942fede2e4473b
leejungyan08 <jungyan.lee@polyware.com.my>
2025-07-07 11:53:59 +0800

    update

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('einvoice:auth')->everyMinute()->withoutOverlapping()->evenInMaintenanceMode();
 Schedule::command('einvoice:check-status --limit=20')->everyMinute()->withoutOverlapping();
 Schedule::command('einvoice:sync-cancel-doc')->everyMinute()->withoutOverlapping();
-Schedule::command('einvoice:sync-man-iss-doc')->hourly()->withoutOverlapping();
