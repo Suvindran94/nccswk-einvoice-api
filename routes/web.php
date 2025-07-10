984a4e476a91e5d402c5f8dee731b4003f88715c
leejungyan08 <jungyan.lee@polyware.com.my>
2025-07-10 12:28:24 +0800

    update env

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,4 @@
 
 use App\Http\Controllers\EInvoiceController;
 use Illuminate\Support\Facades\Route;
-use App\Models\EmailPermission;
 Route::get('/schedulerDashboard', [EInvoiceController::class, 'schedulerDashboard']);
