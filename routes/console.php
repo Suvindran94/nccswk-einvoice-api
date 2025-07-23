7b8e481a430deb17c02aed350894cdfec5aa5327
leejungyan08 <jungyan.lee@polyware.com.my>
2025-07-23 18:19:48 +0800

    include invalid document scheduler

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('einvoice:auth')->everyMinute()->withoutOverlapping()->evenInMaintenanceMode();
 Schedule::command('einvoice:check-status --limit=20')->everyMinute()->withoutOverlapping();
 Schedule::command('einvoice:sync-cancel-doc')->everyMinute()->withoutOverlapping();
+Schedule::command('einvoice:sync-invalid-doc-inf')->everyMinute()->withoutOverlapping();
